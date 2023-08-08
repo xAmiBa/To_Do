ec5ab9bdd1529e7ed0c3e01ba1da0b43f993a01b
xAmiBa <aminaba666@gmail.com>
2023-08-08 23:28:36 +0100

    replace if statement condition if command in user_input[:4] with if user_input.startswith( command)

--- To_do_list.py
+++ To_do_list.py
@@ -1,4 +1,6 @@
 
+#TODO: list comprehension improvement
+
 todo_list = [] 
 #add list to todo_list temporary variable so next time when program run it reads
 #stored list
@@ -28,11 +30,13 @@ def print_list():
         print(item) 
 
 while True:
-    choice_1 = input("Type add, show, edit, complete or exit: ")
+    #TODO: add better formated menu
+    #TODO: option to choose the list?
+    user_choice = input("Type add, show, edit, complete or exit: ")
     
-    match choice_1:
-        case "add":
-            new_todo = input("Enter a todo: ")
+    if user_choice.startswith("add"):
+        if user_choice[4:] != "":
+            new_todo = user_choice[4:]
             new_todo = box + new_todo.upper()  #converts list into upper case
             todo_list.append(new_todo)  #update on the list
             
@@ -40,14 +44,20 @@ while True:
             for item in todo_list:
                 file.writelines(f"{item}\n")
             file.close()
+        elif user_choice[4:] == "":
+            print("New Todo is not valid!")
 
-        case "show":
-            print_list()
+    elif user_choice.startswith("show"):
+        print_list()
 
-        case "edit":
+    elif user_choice.startswith("edit"):
+        if user_choice != "edit":
+            print("Command is not valid! Type just 'edit'")
+        
+        else:
             print_numbered_list()
 
-            #do this to todo_list
+            #user choose which item
             choice_edit = input("Which item would you like to edit? Number: ")
             choice_edit = int(choice_edit) - 1
             replace_item = input(f"You are editing \"{todo_list[choice_edit]}\". New version: ")
@@ -68,60 +78,57 @@ while True:
             #print file
             print_list()
 
-#FIXME: new run, new list?!
+    elif user_choice.startswith("complete"):
+        print_numbered_list()
+        complete_choice = input("Which todo would you like to complete? Number: ")
+        
+        for index, item in enumerate(todo_list, 1):
+            if index != int(complete_choice):
+                print(item)
+            else:
+                item = item.replace("[ ] ", "[x] ")
+                complete_choice = int(complete_choice)
+                todo_list[complete_choice - 1] = item  #update on the list
+        
+        #clear the txt file
+        with open("todos.txt",'w') as file:
+            pass
 
-        case "complete":
-            print_numbered_list()
-            complete_choice = input("Which todo would you like to complete? Number: ")
-            
-            for index, item in enumerate(todo_list, 1):
-                if index != int(complete_choice):
-                    print(item)
-                else:
-                    item = item.replace("[ ] ", "[x] ")
-                    complete_choice = int(complete_choice)
-                    todo_list[complete_choice - 1] = item  #update on the list
-            
-            #clear the txt file
-            with open("todos.txt",'w') as file:
-                pass
+        #write new todo_list again in the file
+        file = open('todos.txt', 'w')  #update in the file
+                    
+        for item in todo_list:
+            file.writelines(f"{item}\n")
+        file.close()
 
-            #write new todo_list again in the file
-            file = open('todos.txt', 'w')  #update in the file
-                        
-            for item in todo_list:
-                file.writelines(f"{item}\n")
-            file.close()
-
-            #print file
-            print_list()
+        #print file
+        print_list()
 
 #TODO: move completed task to the end?
 
-        case "exit":
-            cleanup_list = input("Would you like to clean up your list? yes/no: ")
+    elif user_choice.startswith("exit"):
+        cleanup_list = input("Would you like to clean up your list? yes/no: ")
 
-            if cleanup_list == "yes":
-                for item in todo_list:
-                    if "[x]" in item:
-                        todo_list.remove(item)
-            
-            #clear the txt file
-            with open("todos.txt",'w') as file:
-                pass
-
-            #write new todo_list again in the file
-            file = open('todos.txt', 'w')  #update in the file
-                        
+        if cleanup_list == "yes":
             for item in todo_list:
-                file.writelines(f"{item}\n")
-            file.close()
+                if "[x]" in item:
+                    todo_list.remove(item)
+        
+        #clear the txt file
+        with open("todos.txt",'w') as file:
+            pass
 
-            #print file
-            print_list()
+        #write new todo_list again in the file
+        file = open('todos.txt', 'w')  #update in the file
+                    
+        for item in todo_list:
+            file.writelines(f"{item}\n")
+        file.close()
 
-            break
+        #print file
+        print_list()
 
+        break
 
-
-
+    else:
+        print("Command is not valid!")
